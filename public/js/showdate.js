a8cf6644b8b636ee45ab71e85b22c77c6bf727ed
TMc42 <tmclean1212@gmail.com>
2020-10-08 17:14:53 -0500

    Added to json

--- public/js/showdate.js
+++ public/js/showdate.js
@@ -3,28 +3,29 @@ function showdate() {
     y = n.getFullYear();
     m = n.getMonth() + 1;
     d = n.getDate();
-    document.getElementById("date").innerHTML = m + "-" + d + "-" + y;
+    document.getElementById("date").innerHTML += "<br>" + m + "-" + d + "-" + y;
 }
 
 function showday(){
     var d = new Date();
 
     var week = new Array(7);
-    week[0] = "sunday";
-    week[1] = "monday";
-    week[2] = "tuesday";
-    week[3] = "wednesday";
-    week[4] = "thursday";
-    week[5] = "friday";
-    week[6] = "saturday";
-    week[7] = "sunday";
+    week[0] = "Sunday";
+    week[1] = "Monday";
+    week[2] = "Tuesday";
+    week[3] = "Wednesday";
+    week[4] = "Thursday";
+    week[5] = "Friday";
+    week[6] = "Saturday";
+    week[7] = "Sunday";
 
     var tmp = week[d.getDay()];
 
     document.getElementById(tmp).style.backgroundColor = "#438A5E";
+    document.getElementById("date").innerHTML = tmp;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    showdate();
     showday();
+    showdate();
 });
--- public/js/todayapplet.js
+++ public/js/todayapplet.js
@@ -1,10 +1,5 @@
 // Code to manipulate the Today section of the site
 
-var todaylist = document.getElementsByTagName("LI");
-for (var i=0;i<todaylist;i++) {
-    var span = document.createElement("SPAN");
-    var text = document.createTextNode("\u00D7");
-    span.className = "completebutton";
-    span.appendChild(txt);
-    todaylist[i].appendChild(span);
+function closeItem(arg) {
+    document.getElementById(arg).style.display="none";
 }
