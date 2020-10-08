db39d3278458e37be3262b86dcc9b73e6b50a764
TMc42 <tmclean1212@gmail.com>
2020-10-08 13:57:51 -0500

    Added JSON file

--- public/js/showdate.js
+++ public/js/showdate.js
@@ -6,6 +6,25 @@ function showdate() {
     document.getElementById("date").innerHTML = m + "-" + d + "-" + y;
 }
 
+function showday(){
+    var d = new Date();
+
+    var week = new Array(7);
+    week[0] = "sunday";
+    week[1] = "monday";
+    week[2] = "tuesday";
+    week[3] = "wednesday";
+    week[4] = "thursday";
+    week[5] = "friday";
+    week[6] = "saturday";
+    week[7] = "sunday";
+
+    var tmp = week[d.getDay()];
+
+    document.getElementById(tmp).style.backgroundColor = "#438A5E";
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     showdate();
+    showday();
 });
