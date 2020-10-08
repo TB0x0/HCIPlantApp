c3cf69d6970cd4cef1c7746aa4b091c8ba015464
TMc42 <tmclean1212@gmail.com>
2020-10-08 11:05:06 -0500

    added elemnts to calendar

--- public/js/todayapplet.js
+++ public/js/todayapplet.js
@@ -1,6 +1,6 @@
 // Code to manipulate the Today section of the site
 
-var todaylist = document.getElementsByName("today");
+var todaylist = document.getElementsByTagName("LI");
 for (var i=0;i<todaylist;i++) {
     var span = document.createElement("SPAN");
     var text = document.createTextNode("\u00D7");
