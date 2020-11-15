5ffd96cb896002f178859435159520a5c12e2200
TMc42 <tmclean1212@gmail.com>
2020-11-15 14:43:58 -0600

    test

--- public/js/plantly.js
+++ public/js/plantly.js
@@ -1,12 +1,30 @@
-var config = {
+const firebaseConfig = {
     apiKey: "AIzaSyBPyrECQrG0sU6CIvvsn7xebBg51vikQEY",
     authDomain: "plantly-hci.firebaseapp.com",
     databaseURL: "https://plantly-hci.firebaseio.com",
+    projectId: "plantly-hci",
     storageBucket: "plantly-hci.appspot.com",
+    messagingSenderId: "493354786680",
+    appId: "1:493354786680:web:33edebde76104b522f728d"
   };
   
   // Initialize your Firebase app
-  firebase.initializeApp(config);
+  firebase.initializeApp(firebaseConfig);
   
   // Reference to your entire Firebase database
   var myFirebase = firebase.database().ref();
+
+  function pushPlant(){
+    firebase.database().ref('users/' + userId).set({
+        username: name,
+        email: email,
+        profile_picture : imageUrl
+      }, function(error) {
+        if (error) {
+          // The write failed...
+        } else {
+          // Data saved successfully!
+        }
+      });
+    }
+  }
